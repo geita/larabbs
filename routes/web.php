cb5d06db809fb5e7b1cd35229d0173a7106ff9b1
man  hua <zhangfby@163.com>
2018-12-04 15:43:19 +0000

    User profile page

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,12 @@ Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm'
 Route::post('password/email', 'Auth\forgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+//用户管理之 显示个人信息 显示编辑个人资料页面 处理edit页面提交
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+//等同于以下代码
+/*Route::get('/users/{users}', 'UsersController@show')->name('users.show');
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+Route::patch('/users/{user}', 'UsersController@update')->name('users.update');*/
+

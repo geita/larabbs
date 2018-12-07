8e6bfea108bd979fae9c2882fc68ea31071cdb59
man  hua <zhangfby@163.com>
2018-12-07 16:52:54 +0000

    User can upload avatar

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {   
@@ -28,9 +29,23 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    /**
+     * 更新用户
+     * @param  UserRequest $request [description]
+     * @param  User        $user    [description]
+     * @return [type]               [description]
+     */
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
-        $user->update($request->all());
+        $data = $request->all();
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatar', $user->id, 400);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
---
+++ config/default.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'avatar' => env('DEFAULT_AVATAR', 'https://iocaffcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/600/h/600'),
+];
+
+?>

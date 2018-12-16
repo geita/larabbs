374bbdfa7e19de9fcd40f0fad2bef9e1d2107c81
man  hua <zhangfby@163.com>
2018-12-16 04:45:01 +0000

    Add queue translate slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Models\Topic;
 use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
@@ -19,14 +20,42 @@ class TopicObserver
         //
     }
 
+    /**
+     * 创建成功前
+     * @Author   manhua
+     * @DateTime 2018-12-16
+     * @param    [array]
+     * @param    [object]
+     * @param    Topic      $topic [description]
+     * @return   [type]            [description]
+     */
     public function saving(Topic $topic)
     {
         //生成话题摘要    
         $topic->excerpt = make_excerpt($topic->body);
 
+    }
+
+    /**
+     * 创建成功后
+     * @Author   manhua
+     * @DateTime 2018-12-16
+     * @param    [array]
+     * @param    [object]
+     * @param    Topic      $topic [description]
+     * @return   [type]            [description]
+     */
+    public function saved(Topic $topic)
+    {
         //如果slug 字段无内容， 即使用翻译器 title 进行翻译
          if (!$topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            //直接请求 存储
+            //$topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            
+            //推送任务到队列中
+            dispatch(new TranslateSlug($topic));
+            
          }
     }
 }
+
